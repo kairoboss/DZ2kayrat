71144e2743b5ed36270122292ad0797f4b142bb5
kairoboss <kairoboss125@gmail.com>
2020-06-12 01:21:13 +0600

    Исправил надеюсь.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,28 +2,39 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-        conditions1(50, 25);
-        conditions1(55, 30);
-        conditions1(35, 35);
-        conditions1(25, 28);
-        conditions1(18, 16);
-    }
-    public static String conditions1(int age, int temperature ){
-        String result;
-        if (temperature <30 && temperature > -20 && age > 20 && age < 45 )
-            result = "Можно идти гулять";
 
-        else if (temperature <28 && temperature > 0 && age < 20)
+
+       String elderly = conditions1(50, 25);
+        System.out.println(elderly);
+        String old = conditions1(55, 30);
+        System.out.println(old);
+        String adult = conditions1(35, 35);
+        System.out.println(adult);
+        String young = conditions1(25, 28);
+        System.out.println(young);
+       String teenager = conditions1(18, 16);
+        System.out.println(teenager);
+
+
+
+    }
+
+    public static String conditions1(int age, int temperature) {
+String result;
+        if (temperature < 30 && temperature > -20 && age > 20 && age < 45)
             result = "Можно идти гулять";
-        else if (age > 45 && temperature > -10 && temperature <25)
+        else if (temperature < 28 && temperature > 0 && age < 20)
             result = "Можно идти гулять";
-        else result = "Оставайтесь дома";
-        System.out.println(age+ "лет");
-        System.out.println(temperature + "градусов");
-        System.out.println(result);
-        return result;
+        else if (age > 45 && temperature > -10 && temperature < 25)
+            result = "Можно идти гулять";
+        else
+            result = "Оставайтесь дома";
+
+return result;
+
     }
 }
 
